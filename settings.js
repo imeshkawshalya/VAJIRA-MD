d132314a7acc1b8f127ee139ab0c0ea7e5ba7a46
imeshkawshalya <155098034+imeshkawshalya@users.noreply.github.com>
2024-07-20 11:44:47 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,10 +8,10 @@ function convertToBool(text, fault = 'true') {
 global.thumb = fs.readFileSync('./lib/bugs/venom.jpg')
 const BOTNAME = 'VAJIRA-MD|-WA-BOT™';
 const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
-global.owner = process.env.OWNER_NUMBER  || '94768704257' ;  // ADD YOUR NUMBER WITHOUT +
+global.owner = process.env.OWNER_NUMBER  || '994 4055 79619' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=6ZV0CBKY#gjtT1TB3jlKH8XTbNosl6_B5FKrC2ArLZtARVw2YiUA' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=rQ1UgIjT#a0v55bXxdgjXFyasMqxj52e2mD0J5F57dlXlxikNz6o' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
